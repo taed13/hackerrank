b85b1b42260b216885c5b4838915376574b9e8d8
taed13 <tiendatpot@gmail.com>
2023-04-05 12:41:48 +0700

    day 1

---
+++ JavaCurrencyFormatter.java
@@ -0,0 +1,11 @@
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
